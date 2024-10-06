3bdd24d83a350fb11c8009fa227754cca42a0415
Shaheen Karodia <shaheen.karodia.dev@gmail.com>
2024-10-06 18:17:16 +0200

    Added posts and usersQuery

--- src/common/react-query.ts
+++ src/common/react-query.ts
@@ -1,3 +1,5 @@
 export const QUERY_KEYS = {
-  USER_POSTS: "user-posts",
+  USER_POSTS: ["user-posts"] as const,
+  USERS: ["users"] as const,
+  POSTS: ["posts"] as const,
 };
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -2,8 +2,17 @@ import { NextPage } from "next";
 import axios from "axios";
 import { dehydrate, QueryClient, useQuery } from "@tanstack/react-query";
 import { QUERY_KEYS } from "@/common/react-query";
+import { BASE_URL } from "@/common/constants";
 
-//TODO type post
+/**
+ * TYPES
+ */
+type PaginatedAPI = {
+  total: number;
+  skip: number;
+  limit: number;
+};
+
 type Post = {
   id: number;
   title: string;
@@ -17,21 +26,39 @@ type Post = {
   views: number;
 };
 
-type UserPostsResponse = {
+type User = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  username: string;
+  address: {
+    state: string;
+    country: string;
+  };
+  company: {
+    department: string;
+  };
+};
+
+type PostsAPIResponse = {
   posts: Post[];
-  total: number;
-  skip: number;
-  limit: number;
-};
+} & PaginatedAPI;
 
-const fetchUserPosts = (id: string): Promise<UserPostsResponse[]> =>
-  axios.get(`https://dummyjson.com/users/${id}/posts`).then((res) => res.data);
+type UsersAPIResponse = {
+  users: User[];
+} & PaginatedAPI;
 
-const getPosts = () => {
-  return fetch("https://dummyjson.com/posts")
-    .then((res) => res.json())
-    .then((data) => data);
-};
+/**
+ * FETCHERS
+ */
+const fetchUserPosts = (id: string): Promise<PostsAPIResponse> =>
+  axios.get(`${BASE_URL}/users/${id}/posts`).then((res) => res.data);
+
+const fetchUsers = (): Promise<UsersAPIResponse> =>
+  axios.get(`${BASE_URL}/users`).then((res) => res.data);
+
+const fetchPosts = (): Promise<PostsAPIResponse[]> =>
+  axios.get(`${BASE_URL}/posts`).then((res) => res.data);
 
 const Profile: NextPage = () => {
   const { data } = useQuery({
@@ -40,8 +67,18 @@ const Profile: NextPage = () => {
   });
 
   const userPostsQuery = useQuery({
-    queryKey: [QUERY_KEYS.USER_POSTS],
+    queryKey: QUERY_KEYS.USER_POSTS,
     queryFn: () => fetchUserPosts("1"), // TODO remove hard coding of this value
+  });
+
+  const usersQuery = useQuery({
+    queryKey: QUERY_KEYS.USERS,
+    queryFn: () => fetchUsers(),
+  });
+
+  const postsQuery = useQuery({
+    queryKey: QUERY_KEYS.POSTS,
+    queryFn: () => fetchPosts(),
   });
 
   return (
@@ -51,8 +88,14 @@ const Profile: NextPage = () => {
         {JSON.stringify(data)}
       </h1>
 
-      <p>Recent</p>
+      <p>User Posts</p>
       <pre>{JSON.stringify(userPostsQuery.data)}</pre>
+
+      <p>Users</p>
+      <pre>{JSON.stringify(usersQuery.data)}</pre>
+
+      <p>Posts</p>
+      <pre>{JSON.stringify(postsQuery.data)}</pre>
     </div>
   );
 };
@@ -68,7 +111,7 @@ export async function getServerSideProps() {
 
   await queryClient.prefetchQuery({
     queryKey: ["profile"],
-    queryFn: getPosts,
+    queryFn: fetchPosts,
   });
 
   return {
